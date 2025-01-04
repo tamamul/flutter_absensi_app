8a947ce38bd6a95814077ab45e289d144eaf8f16
andikarafon <andika.rafon.senohaji@gmail.com>
2025-01-05 03:57:00 +0700

    register face update

--- lib/presentation/home/pages/main_page.dart
+++ lib/presentation/home/pages/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_absensi_app/presentation/home/pages/home_page.dart';
 import 'package:flutter_absensi_app/presentation/home/pages/setting_page.dart';
 
 import '../../../core/core.dart';
@@ -13,8 +14,7 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   int _selectedIndex = 0;
   final _widgets = [
-    // const HomePage(),
-    const Center(child: Text('This is home page')),
+    const HomePage(),
     const Center(child: Text('This is history page')),
     const SettingPage(),
     const Center(child: Text('This is profile page')),
